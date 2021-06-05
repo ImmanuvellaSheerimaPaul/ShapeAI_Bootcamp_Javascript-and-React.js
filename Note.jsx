06c5017d36ea16f8eee71cfd87e0f55bb1171169
ImmanuvellaSheerimaPaul <85392155+ImmanuvellaSheerimaPaul@users.noreply.github.com>
2021-06-05 15:54:47 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        7 Days, Certified, Free Web development with Javascript & React.Js (25th
+        May to 31th May) conducted by Shaurya Sinha. This bootcamp covers Web
+        development and Introduction to programming in Javascript and
+        React.js,Hands on Project and much more..
+      </p>
+    </div>
+  );
+}
+export default Note;
